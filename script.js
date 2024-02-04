a4e27068f8548158280fe76e4525b1b14fdac032
Balqees Awawdeh <balqisawawde@gmail.com>
2024-02-04 18:53:57 +0200

    initialize commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function city() {
+    alert("Loading weather report...");    
+}
+function dismiss(){
+var fixedelement = document.querySelector("#cookies")
+fixedelement.remove();
+}
+function test(){
+    document.getElementById("#cookies").style.position = "fixed";
+}
+
+var element1 = document.querySelector("#e1")
+var element2 = document.querySelector("#e2")
+var element3 = document.querySelector("#e3")
+var element4 = document.querySelector("#e4")
+var element5 = document.querySelector("#e5")
+var element6 = document.querySelector("#e6")
+var element7 = document.querySelector("#e7")
+var element8 = document.querySelector("#e8")
+function myFunction(){
+    element1.innerHTML="75ْ "
+    element2.innerHTML="55ْ "
+    element3.innerHTML="80ْ "
+    element4.innerHTML="66ْ "
+    element5.innerHTML="69ْ "
+    element6.innerHTML="61ْ "
+    element7.innerHTML="78ْ "
+    element8.innerHTML="70ْ "
+}
